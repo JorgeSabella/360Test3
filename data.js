d8be72a8f69647ed29cfb941035e2ce65e43bec7
jorgesabella <jorge_andres1996@hotmail.com>
2020-07-29 19:45:43 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,282 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010001",
+      "name": "R0010001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.3157138054334432,
+        "pitch": 0.14396733125155237,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.37109464015183846,
+          "pitch": 0.3286247565448175,
+          "rotation": 0,
+          "target": "1-r0010002"
+        },
+        {
+          "yaw": -1.067658716957407,
+          "pitch": 0.2843505539325193,
+          "rotation": 11.780972450961727,
+          "target": "2-r0010003"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8695983471126514,
+          "pitch": -0.24584384461634556,
+          "title": "Mini Split",
+          "text": "Todos lo cuartos cuentan con mini split integrado<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-r0010002",
+      "name": "R0010002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.3781768418096565,
+        "pitch": 0.47533332564135655,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.65506118613278,
+          "pitch": 0.4558545020013902,
+          "rotation": 0,
+          "target": "2-r0010003"
+        },
+        {
+          "yaw": 2.54326778529403,
+          "pitch": 0.33830878737454384,
+          "rotation": 0,
+          "target": "0-r0010001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r0010003",
+      "name": "R0010003",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.28999316802368114,
+        "pitch": 0.10607879483366212,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.012892642033095,
+          "pitch": 0.41346094464844185,
+          "rotation": 0,
+          "target": "1-r0010002"
+        },
+        {
+          "yaw": 3.112326855042859,
+          "pitch": 0.24525546088230143,
+          "rotation": 0,
+          "target": "0-r0010001"
+        },
+        {
+          "yaw": -0.4215923199074538,
+          "pitch": 0.5119762157885859,
+          "rotation": 0,
+          "target": "3-r0010004"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-r0010004",
+      "name": "R0010004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.378234725805621,
+        "pitch": 0.21627844510571848,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9165638339078068,
+          "pitch": 0.3121269440496022,
+          "rotation": 0,
+          "target": "2-r0010003"
+        },
+        {
+          "yaw": -0.2998474716592838,
+          "pitch": 0.4797967107667471,
+          "rotation": 0,
+          "target": "4-r0010005"
+        },
+        {
+          "yaw": 1.4186822072328749,
+          "pitch": 0.42595997851586276,
+          "rotation": 0,
+          "target": "5-r0010006"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-r0010005",
+      "name": "R0010005",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7329767692466254,
+          "pitch": 0.6207598540231487,
+          "rotation": 0,
+          "target": "3-r0010004"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-r0010006",
+      "name": "R0010006",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.6332100995856287,
+        "pitch": 0.42371761245589035,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.830631090122367,
+          "pitch": 0.6629100143785429,
+          "rotation": 5.497787143782138,
+          "target": "4-r0010005"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Cuarto de Jorge Andres",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
